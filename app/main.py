ecf096d9704a19e7fd76e1d30fb2c1c94905e4a2
Aishwarya B <aishwarya.bandaru@gmail.com>
2023-01-29 23:24:39 +0000

    Update display output to use PyQt6

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@
 # 2) Menu is opened for the user, can update settings eg: their primary and 
 # projector displays and other settings
 
-
+import sys
+import time
 import numpy as np
 import cv2
+from PyQt6 import QtWidgets, QtGui
 
 from utils.main_menu import MainMenu
 from utils.settings_access import SettingsAccess
@@ -12,12 +14,18 @@ from utils.settings_access import SettingsAccess
 from utils.display_output import DisplayOutput
 from utils.display_capture import DisplayCapture
 from utils.audio_capture import AudioCapture
+from utils.tv_detection import TVDetection
 
 from utils.fps import FPS
 
 from projection_modes.modes_factory import ModesFactory
 
-BACKGROUND_IMAGE = "assets/room_image/TV_box.jpeg"
+
+img_path = (__file__[:__file__.index("app") 
+            + len("app")]+"/assets/room_image/TV_box.jpeg")
+BACKGROUND_IMG = cv2.imread(img_path)
+
+print("image = ", BACKGROUND_IMG)
 
 
 def main():
@@ -54,37 +62,57 @@ def main():
 
     display_capture = DisplayCapture(primary_bounding_box, projector_bounding_box)
     audio_capture = AudioCapture()
-    display_output = DisplayOutput()
+    # display_output = DisplayOutput()
+    # # tv_detection = TVDetection()
 
     #Create instance of FPS
     fps = FPS()
 
     #Create the mode objects from the mode factory
-    mode_factory = ModesFactory(display_capture,audio_capture)
+    mode_factory = ModesFactory(BACKGROUND_IMG, display_capture, audio_capture, settings_access)
     mode_object = mode_factory.get_mode(selected_mode)
-    
 
-    #Main loop of application
-    stopped = False
-    while not(stopped):
 
-        #fps.print_fps()
+    # Create PyQt app
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = DisplayOutput()
+    main_window.showFullScreen()
+    # main_window.show()
 
-        #Modes control whether they need screen or audio capture
-        #Calling trigger on the mode object causes it to activate itself and run as required,
-        #trigger always returns a frame or None
+    # Main loop for app
+    while not main_window.stopped:
         frames = mode_object.trigger()
-        
-        #potentially add the depth mapping here
 
-        stopped = display_output.display_frame(frames)
+        if frames is not None or len(frames) != 0:
+            for frame in frames:
+                height, width = frame.shape[:2]
+                bytes_per_line = frame.strides[0]
+                format_string = settings_access.read_mode_settings(selected_mode, "qImg_format")
+                qImg_format = eval(format_string)
+                qImg = QtGui.QImage(frame.data, width, height, bytes_per_line, qImg_format).rgbSwapped()
+                main_window.label.setPixmap(QtGui.QPixmap(qImg))
+                # main_window.setFixedSize(width, height)
+                app.processEvents()
+                time.sleep(0.1)
+    sys.exit(app.exec())
 
-    # run display output unless exit command received eg: 'q' pressed when on 
-    # projector window
 
-    print("Thank you for using UCL Open Illumiroom V2, Have a great day!")
-    # Clean up and exit.
-    exit()
+    # #Main loop of application
+    # display_output = DisplayOutput()
+    # stopped = False
+    # while not(stopped):
+    #     #fps.print_fps()
+    #     #Modes control whether they need screen or audio capture
+    #     frames = mode_object.trigger()
+    #     #potentially add the depth mapping here
+    #     stopped = display_output.display_frame(frames)
+    # # # run display output unless exit command received eg: 'q' pressed when on 
+    # # # projector window
+
+    # # print("Thank you for using UCL Open Illumiroom V2, Have a great day!")
+    # # Clean up and exit.
+    
+    # exit()
 
 if __name__ == '__main__':
     main()
--- app/utils/display_output.py
+++ app/utils/display_output.py
@@ -4,20 +4,44 @@ from mss import mss
 
 import numpy as np
 from cv2 import imshow, namedWindow, setWindowProperty, waitKey, destroyAllWindows, WINDOW_NORMAL, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN
+import sys
+from PyQt6 import QtWidgets, QtGui, QtCore
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtGui import QPixmap, QImage
+from PyQt6.QtCore import Qt
+import time
 
-class DisplayOutput:
 
+class DisplayOutput(QtWidgets.QMainWindow):
     def __init__(self):
-        pass
+        super().__init__()
+        # self.ui = QMainWindow()
+        self.label = QtWidgets.QLabel(self)
+        self.setCentralWidget(self.label)
+        
+        self.stopped = False
+
+    def closeEvent(self, event):
+        self.stopped = True
+        event.accept()
+    
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Escape or event.text() == QtCore.Qt.Key_Q:
+            self.close()
 
 
-    def display_frame(self, frames):
-        #Setup the display window
-        namedWindow("UCL Open Illumiroom V2", WINDOW_NORMAL)
-        setWindowProperty("UCL Open Illumiroom V2", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN)
-        for frame in frames:
-            imshow('UCL Open Illumiroom V2', frame)
-        if (waitKey(1) & 0xFF) == ord('q'):
-            destroyAllWindows()
-            return True
-        return False
+# class DisplayOutput:
+
+#     def __init__(self):
+#         pass
+
+#     def display_frame(self, frames):
+#         #Setup the display window
+#         namedWindow("UCL Open Illumiroom V2", WINDOW_NORMAL)
+#         # setWindowProperty("UCL Open Illumiroom V2", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN)
+#         for frame in frames:
+#             imshow('UCL Open Illumiroom V2', frame)
+#         if (waitKey(1) & 0xFF) == ord('q'):
+#             destroyAllWindows()
+#             return True
+#         return False
