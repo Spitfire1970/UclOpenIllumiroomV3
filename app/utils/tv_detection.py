38ed3739727c5ede0d5c89c5b148b666ba799b26
Aishwarya B <aishwarya.bandaru@gmail.com>
2023-01-29 23:30:53 +0000

    Update TV detection to write to JSON mode_settings

--- app/utils/tv_detection.py
+++ app/utils/tv_detection.py
@@ -1,31 +1,47 @@
 # import cv2
 # Note: window displayed using cv2.imshow, TODO: change this
+from utils.settings_access import SettingsAccess
 
+# TODO: consider other modes like snow
+MODE = "wobble"
 
 class TVDetection():
-    def __init__(self, start, end, image) -> None:
-        self.start = start
-        self.end = end
+    def __init__(self, image) -> None:
         self.img = image
+        self.top_left = None
+        self.bottom_right = None
+    
+    def update_mode_settings(self, mode, settings, new_data):
+        settings_access = SettingsAccess()
+        mode_settings_json = settings_access.read_settings("mode_settings.json")
+
+        for i in range(len(settings)):
+            mode_settings_json[mode][settings[i]] = new_data[i]
+        
+        settings_access.write_settings(self, "mode_settings.json", mode_settings_json)
+        # return mode_settings_json[mode][setting]
+
 
     def on_mouse(self, event, x, y, flags, params):
         # Allow user to create a boundary box for the TV
         if event == cv2.EVENT_LBUTTONDOWN:
-            self.start = (x, y)
+            self.top_left = (x, y)
         elif event == cv2.EVENT_LBUTTONUP:
-            self.end = (x, y)
-            cv2.rectangle(self.img, self.start, self.end, (0, 255, 0), 2)
+            self.bottom_right = (x, y)
+            cv2.rectangle(self.img, self.top_left, self.bottom_right, (0, 255, 0), 2)
             cv2.imshow("Detect TV", self.img)
 
     def calc_center(self):
         # Calculate the center of the TV
-        center_x = int((self.start[0] + self.end[0]) / 2)
-        center_y = int((self.start[1] + self.end[1]) / 2)
+        center_x = int((self.top_left[0] + self.bottom_right[0]) / 2)
+        center_y = int((self.top_left[1] + self.bottom_right[1]) / 2)
         # TODO: handle exceptions
-        f = open("TV_info.txt", "w")
-        f.write(f"{center_x}  {center_y}  {self.start}  {self.end}")
-        f.close()
-        return center_x, center_y, self.start, self.end
+
+        settings = ["tv_top_left", "tv_bottom_right", "tv_center_x", "tv_center_y"]
+        new_values = [self.top_left, self.bottom_right, center_x, center_y]
+
+        self.update_mode_settings(MODE, settings, new_values)
+        # return center_x, center_y
 
     def detect_tv(self):
         cv2.namedWindow("Detect TV")
@@ -37,4 +53,4 @@ class TVDetection():
                 break
 
         cv2.destroyAllWindows()
-        return self.calc_center()
+        self.calc_center()
