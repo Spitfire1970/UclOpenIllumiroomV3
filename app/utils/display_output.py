1a8f4dd9dfdd9221784f227783b90441eb09b748
zcabfab <zcabfab@ucl.ac.uk>
2023-02-07 14:28:02 +0000

    Small changes fix bug with image display

--- app/utils/display_output.py
+++ app/utils/display_output.py
@@ -17,26 +17,28 @@ class DisplayOutput(QtWidgets.QMainWindow):
     def __init__(self, primary_bounding_box, projector_bounding_box):
         super().__init__()
         # self.ui = QMainWindow()
+
+        
         self.primary_bounding_box = primary_bounding_box
         self.projector_bounding_box = projector_bounding_box
 
         self.exit_key_binding = Qt.Key.Key_Escape.value
-        display_monitor = 1
-
-        monitors = QScreen.virtualSiblings(self.screen())
-        monitor = monitors[display_monitor].availableGeometry()
               
         self.label = QtWidgets.QLabel(self)
         self.setCentralWidget(self.label)
+        self.setWindowTitle("UCL Open-Illumiroom V2")
 
-        #Move to the mss coordinates of the projector 
-        projector_left = projector_bounding_box['left']
-        projector_top = projector_bounding_box['top']
 
-        self.move(projector_left, projector_top)
+        # #Move to the mss coordinates of the projector 
+        # projector_left = int(projector_bounding_box['left'] )
+        # projector_top = projector_bounding_box['top']
+
+        self.move(projector_bounding_box['left'], projector_bounding_box['top'])
         self.showFullScreen()
+        #self.show()
 
         print("Window Opened, press Escape in the illumiroom window to exit")
+        print("If you have an issue with the image fitting, please ensure that your projector is at 100% scaling")
         
         self.stopped = False
         return 
@@ -49,19 +51,3 @@ class DisplayOutput(QtWidgets.QMainWindow):
             self.close()
             exit()
 
-
-# class DisplayOutput:
-
-#     def __init__(self):
-#         pass
-
-#     def display_frame(self, frames):
-#         #Setup the display window
-#         namedWindow("UCL Open Illumiroom V2", WINDOW_NORMAL)
-#         # setWindowProperty("UCL Open Illumiroom V2", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN)
-#         for frame in frames:
-#             imshow('UCL Open Illumiroom V2', frame)
-#         if (waitKey(1) & 0xFF) == ord('q'):
-#             destroyAllWindows()
-#             return True
-#         return False
