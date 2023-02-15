21ee1d89504778018cd6c0f6f98eec00e1d6e62c
zcabfab <zcabfab@ucl.ac.uk>
2023-02-15 19:07:42 +0000

    commented out libraries

--- app/utils/audio_capture.py
+++ app/utils/audio_capture.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pyaudio
-import soundcard as sc
-import soundfile as sf
-# import os
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from keras.models import load_model
-import librosa
+# import soundcard as sc
+# import soundfile as sf
+# # import os
+# # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# from keras.models import load_model
+# import librosa
 
 
 class AudioCapture():
