df1aef4741f3dc7d8f7cbe52a277b05fe6f53712
Damian <damian.ziaber99@gmail.com>
2023-03-08 21:41:44 +0100

    add functionality to calibration flow

--- app/utils/calibration/calibration.py
+++ app/utils/calibration/calibration.py
@@ -1,188 +1,180 @@
-from cv2 import (imread, FONT_HERSHEY_SIMPLEX, putText, imshow, namedWindow,
-WINDOW_GUI_NORMAL, moveWindow, waitKey, destroyAllWindows, imwrite)
+import itertools
+import os
 from ctypes import *
-import numpy as np
+from dataclasses import dataclass
+
+import cv2
 
 from .threaded_video_capture import ThreadedVideoCapture
 
+confirm_img_text_1 = "Is this the image from the correct webcam? If it is, press 'y' to continue."
+confirm_img_text_2 = "If it is not, especially if a black screen is shown, press 'n'."
+
+
+def _add_confirm_text_to_image(img):
+    img_copy = img.copy()
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    textLocation1 = (50, 50)
+    textLocation2 = (50, 100)
+    fontScale = 0.8
+    fontColor = (255, 0, 0)
+    thickness = 3
+    lineType = 2
+
+    cv2.putText(img_copy, confirm_img_text_1,
+                textLocation1,
+                font,
+                fontScale,
+                fontColor,
+                thickness,
+                lineType)
+
+    cv2.putText(img_copy, confirm_img_text_2,
+                textLocation2,
+                font,
+                fontScale,
+                fontColor,
+                thickness,
+                lineType)
+
+    return img_copy
+
 
 class Calibration:
-
     def __init__(self, settings_access, display_capture):
-        self.settings_access = settings_access
+        self.data_folder = settings_access.assets_path + "calibration/grey_code_photos/grey_code"
         self.display_capture = display_capture
-        self.projector_resolution = self.display_capture.get_projector_bounding_box()
         self.cam_port = settings_access.read_general_settings("camera_nr")
-
-        #Only set up video capture once capture called
-        self.video_capture = None
-        
-
-        # self.gcp = cv2structured_light.GrayCodePattern.create(self.projector_resolution["width"],
-        # self.projector_resolution["height"])
-        self.grey_code_image_library_path = self.settings_access.assets_path + "calibration/grey_code_photos/grey_code"
-        self.grey_code_source_library_path = self.settings_access.assets_path + "calibration/grey_code_source/grey_code"
-        self.instructions_image_library_path = self.settings_access.assets_path + "calibration/instructions/"
-        self.room_image_library_path = self.settings_access.assets_path + "room_image/room_img.jpg"
-
-        self.calibration_dll_path = self.settings_access.assets_path + "calibration/ProjectionCalibration.dll"
-
-        self.num_images = 5
-        self.confirmed_webcam = False
-        self.continue_instructions = False
-        self.correct_webcam = False
-
-        self.frame_sleep_time = 1000
-
-        # get intruction images
-        self.instruction_image_names = ["place_webcam.jpg", "camera_select.jpg", "grey_code_capture.jpg",
-                                        "software_calibration.jpg", "calibration_complete.jpg"]
-        self.instruction_images = [
-            self.display_capture.frame_primary_resize(imread(self.instructions_image_library_path + img)) for img in
-            self.instruction_image_names]
-
+        self.calib_dll = cdll.LoadLibrary(settings_access.assets_path + "calibration/dlls/ProjectionCalibration.dll")
         self.primary_bounding_box = self.display_capture.get_primary_bounding_box()
         self.projector_bounding_box = self.display_capture.get_projector_bounding_box()
-
-        self.confirm_img_text_1 = "Is this the image from the correct webcam? If it is, press 'y' to continue."
-        self.confirm_img_text_2 = "If it is not, expecially if a black screen is shown, press 'n'."
-
-        self.captured_frames = []
-
-    def add_confirm_text_to_image(self, img):
-        font = FONT_HERSHEY_SIMPLEX
-        textLocation1 = (50, 50)
-        textLocation2 = (50, 100)
-        fontScale = 0.8
-        fontColor = (255, 0, 0)
-        thickness = 3
-        lineType = 2
-
-        putText(img, self.confirm_img_text_1,
-                    textLocation1,
-                    font,
-                    fontScale,
-                    fontColor,
-                    thickness,
-                    lineType)
-
-        putText(img, self.confirm_img_text_2,
-                    textLocation2,
-                    font,
-                    fontScale,
-                    fontColor,
-                    thickness,
-                    lineType)
-
-    def capture_grey_code_images(self):
-        # grey code insstructions frame 2
-        imshow("Instructions", self.instruction_images[2])
-
-        # set up grey code window
-        namedWindow("GreyCode", flags=WINDOW_GUI_NORMAL)  # gui normal makes fullscreen
-        calib_dll = cdll.LoadLibrary("ProjectionCalibration.dll")
-
-        calib_dll.capture(self.grey_code_image_library_path.encode(),
-                          self.cam_port,
-                          self.projector_bounding_box["width"],
-                          self.projector_bounding_box["height"],
-                          self.primary_bounding_box["left"],
-                          self.primary_bounding_box["top"])
+        instruction_image_names = ["place_webcam.jpg", "camera_select.jpg", "grey_code_capture.jpg",
+                                   "software_calibration.jpg", "calibration_complete.jpg"]
+        self.instruction_images = [
+            self.display_capture.frame_primary_resize(cv2.imread(settings_access.assets_path
+                                                                 + "calibration/instructions/" + img_name))
+            for img_name in instruction_image_names]
 
     def capture(self):
-        
-        self.video_capture = ThreadedVideoCapture(self.cam_port)
+        """
+        Capture gray code pattern frames
+        """
+        # Setup displays and start camera
+        camera = ThreadedVideoCapture(self.cam_port)
+        tv_monitor = Monitor("Instructions",
+                             (self.primary_bounding_box["left"], self.primary_bounding_box["top"]),
+                             (self.primary_bounding_box["width"], self.primary_bounding_box["height"]))
+        tv_monitor.open_fullscreen()
 
-        namedWindow("Instructions")
-        moveWindow("Instructions", self.primary_bounding_box["left"], self.primary_bounding_box["top"])
+        projector_monitor = Monitor("Projector",
+                                    (self.projector_bounding_box["left"], self.projector_bounding_box["top"]),
+                                    (self.projector_bounding_box["width"], self.projector_bounding_box["height"]))
 
-        # show webcam setup instructions, frame 1
-        while not (self.continue_instructions):
-            imshow("Instructions", self.instruction_images[0])
+        # Show first instruction slide
+        tv_monitor.display_image(self.instruction_images[0])
+        while cv2.waitKey(1) != 32:
+            pass
 
-            key = waitKey(1)
-            if key == 32:  # space key
-                self.continue_instructions = True
-        destroyAllWindows()
-        self.continue_instructions = False
+        room_img = camera.read()
+        tv_monitor.display_image(self.display_capture.frame_primary_resize(_add_confirm_text_to_image(room_img)))
 
-        # black_projection, white_projection = self.gcp.getImagesForShadowMasks(self.projector_resolution,self.projector_resolution)
-        test_image = self.video_capture.read()
-        room_image = np.copy(test_image)
-        # check if any image at all was returned
-        imwrite(self.grey_code_image_library_path + "_test.jpg", test_image)
-        #self.video_capture.release()
-        # check if this is the correct webcam
-
-        while not (self.confirmed_webcam):
-            self.add_confirm_text_to_image(test_image)
-            namedWindow("Webcam Output")
-            moveWindow("Webcam Output", self.primary_bounding_box["left"], self.primary_bounding_box["top"])
-            imshow("Webcam Output", self.display_capture.frame_primary_resize(test_image))
-
-            key = waitKey(1)
-            if key == ord("n"):
-                self.correct_webcam = False
-                self.confirmed_webcam = True
-
-                # show webcam selection instructions, frame 1
-                while not (self.continue_instructions):
-                    imshow("Instructions", self.instruction_images[1])
-
-                    key = waitKey(1)
-                    if key == 32:  # space key
-                        self.continue_instructions = True
-                destroyAllWindows()
+        while True:
+            key = cv2.waitKey(1)
+            if key == ord('y'):
+                break
+            if key == ord('n'):
+                camera.close()
+                tv_monitor.display_image(self.instruction_images[1])
+                while cv2.waitKey(1) != 32:
+                    pass
+                cv2.destroyAllWindows()
                 exit()
 
-            elif key == ord("y"):
-                self.correct_webcam = True
-                self.confirmed_webcam = True
-                # write the image taken to the room image, as it is correct, resize to projector resolution
-                # room_image_resized = self.display_capture.resize_image_fit_projector_each_frame(test_image)
-                # print(room_image_resized.shape)
-                imwrite(self.room_image_library_path, room_image)
+        tv_monitor.display_image(self.instruction_images[2])
 
-        destroyAllWindows()
+        gcp = cv2.structured_light.GrayCodePattern.create(self.projector_bounding_box['width'],
+                                                          self.projector_bounding_box['height'])
+        captured_frames = []
 
-        # # get the grey code images from the webcam
-        # if self.correct_webcam:
-        #     room_img = self.video_capture.read()
-        #     self.captured_frames.append(room_img)
-        #     self.capture_grey_code_images()
+        projector_monitor.open_fullscreen()
+        black_projection, white_projection = gcp.getImagesForShadowMasks((self.projector_bounding_box['width'],
+                                                                          self.projector_bounding_box['height']),
+                                                                         (self.projector_bounding_box['width'],
+                                                                          self.projector_bounding_box['height']))
+        projector_monitor.display_image(black_projection)
+        for projection in itertools.chain(gcp.generate()[1], [black_projection, white_projection]):
+            projector_monitor.display_image(projection)
+            cv2.waitKey(300)
+            captured_frames.append(camera.read())
 
-        #calib_dll = cdll.LoadLibrary(self.calibration_dll_path)
-        #calib_dll.calibrate(self.grey_code_image_library_path.encode())
-        # show instructions 3
-        imshow("Instructions", self.instruction_images[3])
-        waitKey(5000)
+        projector_monitor.close()
+        camera.close()
 
-        # calibration complete! instructions 4
-        while not (self.continue_instructions):
-            imshow("Instructions", self.instruction_images[4])
+        if not os.path.exists(self.data_folder):
+            os.makedirs(self.data_folder)
 
-            key = waitKey(1)
-            if key == 32:  # space key
-                self.continue_instructions = True
-        destroyAllWindows()
+        fs = cv2.FileStorage(os.path.join(self.data_folder, "projection_size.ext"), cv2.FILE_STORAGE_WRITE)
+        fs.write("h", self.projector_bounding_box['height'])
+        fs.write("w", self.projector_bounding_box['width'])
+        fs.release()
+        for i, pattern_image in enumerate(captured_frames[:-2]):
+            path = os.path.join(self.data_folder, "pattern" + str(i) + ".png")
+            cv2.imwrite(path, pattern_image)
+        cv2.imwrite(os.path.join(self.data_folder, "blackFrame.png"), captured_frames[-2])
+        cv2.imwrite(os.path.join(self.data_folder, "whiteFrame.png"), captured_frames[-1])
 
-        # calibration complete, exit
+        tv_monitor.display_image(self.instruction_images[3])
+        self.calibrate()
+        tv_monitor.display_image(self.instruction_images[4])
+        while cv2.waitKey(1) != 32:
+            pass
+        tv_monitor.close()
+        cv2.destroyAllWindows()
 
-        # err, img = cam.read()
+    def calibrate(self):
+        """
+        Run the calibration script on this classes data folder
+        """
+        r = cv2.selectROI("Select the TV", cv2.imread(os.path.join(self.data_folder, "blackFrame.png")))
+        cv2.destroyWindow("Select the TV")
+        contour = (c_int * 8)(r[0], r[1], r[0] + r[2], r[1], r[0] + r[2], r[1] + r[3], r[0], r[1] + r[3])
+        self.calib_dll.calibrate(self.data_folder.encode(), contour)
 
-        # print(img)
+    def read_maps(self):
+        """
+        Get remap maps from the data folder
+        :return:
+        """
+        fs = cv2.FileStorage(os.path.join(self.data_folder, "map.ext"), cv2.FILE_STORAGE_READ)
+        map1 = fs.getNode("map1").mat()
+        map2 = fs.getNode("map2").mat()
+        fs.release()
+        return map1, map2
 
-        # waitKey(1000)
-        # black_projection, white_projection = self.gcp.getImagesForShadowMasks(self.projector_resolution,
-        #                                                                     self.projector_resolution)
-        # for projection in itertools.chain(self.gcp.generate()[1], [black_projection, white_projection]):
-        #     imshow("img",projection)
-        #     waitKey(400)
-        #     print("reading camera")
-        #     err, img = cam.read()
-        #     captured_frames.append(img)
-        #     waitKey(400)
-        #     self.monitor.display_image(projection, 1)
 
-        # self.monitor.close()
-        # return captured_frames[:-2], captured_frames[-2], captured_frames[-1]
+@dataclass
+class Monitor:
+    name: str
+    position: tuple[int, int]
+    resolution: tuple[int, int]
+
+    def open_fullscreen(self):
+        """
+        Open a fullscreen window on the monitor
+        """
+        cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
+        cv2.moveWindow(self.name, self.position[0], self.position[1])
+        cv2.setWindowProperty(self.name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    def display_image(self, img):
+        """
+        Display given image on the monitor
+        :param img: Image to display
+        """
+        cv2.imshow(self.name, img)
+
+    def close(self):
+        """
+        Destroy the opencv window
+        """
+        cv2.destroyWindow(self.name)
