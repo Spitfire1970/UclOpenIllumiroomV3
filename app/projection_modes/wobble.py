f37196a28cab5c75a6dcb482ad91adeeea03609c
Aishwarya B <aishwarya.bandaru@gmail.com>
2023-03-14 14:07:42 +0000

    Update wobble's access to mode settings

--- app/projection_modes/wobble.py
+++ app/projection_modes/wobble.py
@@ -106,14 +106,23 @@ class Wobble(Mode):
         ):
         self.img = background_img
         self.settings = settings_access
-        self.num_frames = self.settings.read_mode_settings("wobble", "num_frames")
+
+        self.wobble_settings = self.settings.read_mode_settings("wobble", "data")
+        self.num_frames = self.wobble_settings["num_frames"]
+        self.amplitude = self.wobble_settings["initial_amplitude"]
+        self.frequency = self.wobble_settings["initial_frequency"]
 
         self.output_img = np.zeros_like(self.img)
         height, width = self.img.shape[:2]
         self.x, self.y = np.meshgrid(np.arange(width), np.arange(height))
         self.original_x, self.original_y = self.x.copy(), self.y.copy()
-        self.fractal_amplitude = self.settings.read_mode_settings("wobble", "fractal_amplitude")
+        self.fractal_amplitude = self.wobble_settings["fractal_amplitude"]
 
+        self.tv_data = self.settings.read_mode_settings("wobble", "tv_data")
+        self.tv_tl_coords = self.tv_data["tv_top_left"]
+        self.tv_br_coords = self.tv_data["tv_bottom_right"]
+        self.tv_center_x = self.tv_data["tv_center_x"]
+        self.tv_center_y = self.tv_data["tv_center_y"]
         # self.threshold = self.settings.read_mode_settings("wobble", "sound_threshold")
 
         self.audio_capture = audio_capture
@@ -127,22 +136,17 @@ class Wobble(Mode):
         print("Generating wobble frames, awesomeness coming soon!")
         self.frames = []
         for i in range(self.num_frames):
-            amplitude = 30 * (i/self.num_frames)
-            frequency = 0.10 * (i/self.num_frames)
+            amplitude = self.amplitude * (i/self.num_frames)
+            frequency = self.frequency * (i/self.num_frames)
             # Interpolation factor
-            factor = 1 - i / (self.num_frames-1)
+            factor = 1 - i / (self.num_frames - 1)
 
             # Create a mask to exclude pixels inside the TV rectangle
-            top_left_coords = self.settings.read_mode_settings("wobble", "tv_top_left")
-            bottom_right_coors = self.settings.read_mode_settings("wobble", "tv_bottom_right")
             mask = np.zeros(self.img.shape[:2], dtype=np.uint8)
-            cv2.rectangle(mask, top_left_coords, bottom_right_coors, (255, 255, 255), -1)
+            cv2.rectangle(mask, self.tv_tl_coords, self.tv_br_coords, (255, 255, 255), -1)
 
             # Calculate distance from the center of the effect
-            center_x = self.settings.read_mode_settings("wobble", "tv_center_x")
-            center_y = self.settings.read_mode_settings("wobble", "tv_center_y")
-
-            distance = np.sqrt((self.x - center_x) ** 2 + (self.y - center_y) ** 2)
+            distance = np.sqrt((self.x - self.tv_center_x) ** 2 + (self.y - self.tv_center_y) ** 2)
             distance[mask == 255] = 0
             fractal_noise = np.random.normal(0, self.fractal_amplitude, distance.shape)
             distance = distance + fractal_noise
